f141e0da67d7724f9533d045fcda4bc8403cdc5c
Dhawal Jain <dhawaljain@Dhawals-MacBook-Air.local>
2024-11-19 13:40:28 +0530

    moving slicing

---
+++ Slicing.py
@@ -0,0 +1,12 @@
+'''Write a function return a substring where it starts from 2rd occurrence of ‘a’ and end at
+occurrent of ‘b’
+e.g. s = "abracadabra"
+start_char = 'a'
+end_char = 'b'
+Expected output: acadab'''
+
+slicee='abracadabra'
+def slc():
+ print(slicee[3:9])
+
+slc()
